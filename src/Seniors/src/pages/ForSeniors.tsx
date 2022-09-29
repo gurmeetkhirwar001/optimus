a2761da303fcd13c3d3ebc86bafe91dee221c1a9
AI VECTORS <103539434+AIV-GH@users.noreply.github.com>
2022-09-28 17:11:56 -0700

    Add files via upload

---
+++ src/Seniors/src/pages/ForSeniors.tsx
@@ -0,0 +1,275 @@
+import { FunctionComponent } from "react";
+import styles from "./ForSeniors.module.css";
+import { Link } from "react-router-dom";
+
+const ForSeniors: FunctionComponent = () => {
+  return (
+    <div className={styles.forSeniorsDiv}>
+      <div className={styles.forSeniorsDiv1}>
+        <div className={styles.footerDiv}>
+          <div className={styles.rectangleDiv} />
+          <div className={styles.rectangleDiv1} />
+          <a href="/"><div className={styles.logoDiv}>
+          <img src="logo.png" height="50" width="50" />
+          </div></a>
+          <div className={styles.transformingLivesOfPeopleA}>
+          Transforming lives across the globe by adventuring Artificial Intelligence.
+          </div>
+          <div
+            className={styles.needAnswersNeedHelpJust}
+          >{`Need Answers? Need help ? Just email us `}</div>
+          <div className={styles.copyright2022Div}>Copyright 2022</div>
+          <div className={styles.companyDiv}>Company</div>
+          <Link to="/product"><div className={styles.productDiv}>Product</div></Link>
+          <Link to="/competitors"><div className={styles.competitorsDiv}>Competitors</div></Link>
+          <div className={styles.pricingDiv}>Pricing</div>
+          <div className={styles.contactDiv}>Contact</div>
+          <div className={styles.linksDiv}>Links</div>
+          <div className={styles.followUsDiv}>{`Follow Us `}</div>
+          <div className={styles.getInTouchWithUs}>Get In touch with Us</div>
+          <div className={styles.groupDiv}>
+            <div className={styles.fbcomaaaDiv}>https://twitter.com/AiTensors</div>
+            <a href="https://twitter.com/AiTensors"><img
+              className={styles.featherfacebookIcon}
+              alt=""
+              src="twitter.svg"
+            /></a>
+          </div>
+          <div className={styles.groupDiv1}>
+            <div className={styles.fbcomaaaDiv}>https://www.linkedin.com/company/82789169/</div>
+            <a href="https://www.linkedin.com/company/82789169/"><img
+              className={styles.featherfacebookIcon}
+              alt=""
+              src="linkedin.svg"
+            /></a>
+          </div>
+          
+          <div className={styles.xFormGroupDiv}>
+            <div className={styles.labelDiv}>
+              <div className={styles.helperTextDiv}>Default Label</div>
+            </div>
+            
+            <div className={styles.helperTextDiv1}>
+              <div className={styles.helperTextDiv2}>Helper text</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.homeBannerDiv}>
+          <div className={styles.rectangleDiv2} />
+          <img className={styles.ellipseIcon} alt="" src="../ellipse-42.svg" />
+          <img className={styles.groupIcon1} alt="" src="../group-163.svg" />
+          <div className={styles.groupDiv3}>
+            <div className={styles.image48Div} />
+            <img className={styles.groupIcon2} alt="" src="../group-25.svg" />
+            <div className={styles.groupDiv4}>
+              <img className={styles.iconlyLightNotification} alt="" />
+              <div className={styles.jumpStartDiv}>{`Jump start `}</div>
+              <div className={styles.yourPortfolioLooksGreatTod}>
+                Your portfolio looks great today
+              </div>
+            </div>
+            <div className={styles.groupDiv5}>
+              <div className={styles.groupDiv6}>
+                <div className={styles.portfolioBalanceDiv}>
+                  Portfolio balance
+                </div>
+                <div className={styles.div}>$12,175.11</div>
+                <div className={styles.groupDiv7}>
+                  <img
+                    className={styles.vectorIcon}
+                    alt=""
+                    src="../vector-1.svg"
+                  />
+                  <div className={styles.div1}>$24.74 (3.79%)</div>
+                </div>
+                <div className={styles.todayMay222021}>Today (May 22,2021)</div>
+              </div>
+              <div className={styles.groupDiv8}>
+                <div className={styles.availableBalanceDiv}>
+                  Available balance
+                </div>
+                <div className={styles.div2}>$173.21</div>
+                <div className={styles.groupDiv9}>
+                  <img className={styles.vectorIcon1} alt="" />
+                  <div className={styles.portfolioBalanceDiv}>More details</div>
+                </div>
+              </div>
+              <div className={styles.groupDiv10}>
+                <div className={styles.rectangleDiv3} />
+                <div className={styles.hDiv}>1h</div>
+              </div>
+              <div className={styles.groupDiv11}>
+                <div className={styles.rectangleDiv4} />
+                <div className={styles.hDiv1}>24h</div>
+              </div>
+              <div className={styles.groupDiv12}>
+                <div className={styles.rectangleDiv4} />
+                <div className={styles.wDiv}>1w</div>
+              </div>
+              <div className={styles.groupDiv13}>
+                <div className={styles.rectangleDiv4} />
+                <div className={styles.wDiv}>1w</div>
+              </div>
+              <div className={styles.groupDiv14}>
+                <div className={styles.rectangleDiv4} />
+                <div className={styles.yDiv}>1y</div>
+              </div>
+              <div className={styles.groupDiv15}>
+                <div className={styles.rectangleDiv4} />
+                <div className={styles.wDiv}>All</div>
+              </div>
+              <div className={styles.rectangleDiv9} />
+              <img className={styles.groupIcon3} alt="" src="../group-18.svg" />
+              <div className={styles.groupDiv16}>
+                <div className={styles.rectangleDiv10} />
+                <div className={styles.sendDiv}>Send</div>
+              </div>
+              <div className={styles.groupDiv17}>
+                <div className={styles.rectangleDiv11} />
+                <div className={styles.withdrawDiv}>Withdraw</div>
+              </div>
+              <img className={styles.groupIcon4} alt="" />
+              <img className={styles.groupIcon5} alt="" src="../group-27.svg" />
+            </div>
+          </div>
+          <b className={styles.meetOptimusB}>AI Caregiver for Elders</b>
+          <div
+            className={styles.loremIpsumDolorSitAmetCo}
+          >{`Using Optimus to avoid incidents and monitor behaviour patterns for unusual behaviour in elders allows many seniors to stay in their homes safely. 
+          And notifying caregivers, family, friends and doctors for quick assistance to elders.
+           `}</div>
+          <img
+            className={styles.bluee011Icon}
+            alt=""
+            src="../bluee01-1@2x.png"
+          />
+        </div>
+        <div className={styles.groupDiv18}>
+          <div className={styles.groupDiv19}>
+            <b className={styles.everyVoiceCounts}>
+              <p className={styles.everyVoiceCounts1} font-weight="500">Every voice counts</p>
+            </b>
+            <div className={styles.whatCanYouDoWithOptimus}>
+              <span>
+                {" "}
+                Need to switch off the AC or read out an email? Just voice it to do 
+                something which you usually do yourself by reaching out to the Smartphone or PC or TV.
+              </span>
+            </div>
+          </div>
+        </div>
+        <div className={styles.groupDiv20}>
+          <div
+            className={styles.optimusIsACuttingEdgeVirt}
+          >{`Falls are the leading cause of fatal and nonfatal injuries among adults aged ≥65 years (older adults).
+          Our solution is to monitor elders 24/7 and detect in case of falls only by using images from a standard video-camera without the need to use depth camera nor environmental sensors.
+           `}</div>
+          <b className={styles.fallDetectionB}>Fall Detection</b>
+        </div>
+        <div className={styles.groupDiv21}>
+          <div
+            className={styles.optimusIsACuttingEdgeVirt}
+          >{`Optimus camera detects senior sleep pattern and analyses and sends notification to care taker on daily or weekly.`}</div>
+          <b className={styles.fallDetectionB}>Sleep Behaviour</b>
+        </div>
+        <div className={styles.groupDiv22}>
+          <div
+            className={styles.optimusIsACuttingEdgeVirt}
+          >{`Optimus eyes and voice helps low vision seniors by reading and speaking expiry dates of food packages, and many more.`}</div>
+          <b className={styles.fallDetectionB}>Vision</b>
+        </div>
+        <div className={styles.groupDiv23}>
+          <div
+            className={styles.optimusIsACuttingEdgeVirt}
+          >{`Seniors using tech is not quite often, so Optimus custom skill set (Amazon Alexa) helps checking route, calling cab and many more with simple questions.`}</div>
+          <b className={styles.fallDetectionB}>Transportation</b>
+        </div>
+        <div className={styles.groupDiv24}>
+          <div
+            className={styles.optimusIsACuttingEdgeVirt}
+          >{`Optimus voice alerts seniors about medication, and optimus camera detects medication consumption and notifies care taker if the user is missing a schedule. `}</div>
+          <b className={styles.fallDetectionB}>Medication Schedule</b>
+        </div>
+        <div className={styles.groupDiv25}>
+          <div
+            className={styles.optimusIsACuttingEdgeVirt}
+          >{`Optimus eyes detects senior anomaly behaviors and notifies care taker if necessary. `}</div>
+          <b className={styles.fallDetectionB}>Prediction Anomalies</b>
+        </div>
+        <div className={styles.groupDiv26}>
+          <div
+            className={styles.optimusIsACuttingEdgeVirt}
+          >{`Optimus custom NLP skill set(Amazon Alexa) for seniors, helps understanding nutritions. `}</div>
+          <b className={styles.fallDetectionB}>Nutrition</b>
+        </div>
+        <div className={styles.groupDiv27}>
+          <img className={styles.ezgif6Icon} alt="" src="../ezgif-6@2x.png" />
+        </div>
+        <div className={styles.groupDiv28}>
+          <div className={styles.rectangleDiv12} />
+          <div className={styles.rectangleDiv13} />
+          <div className={styles.rectangleDiv14} />
+          <div className={styles.rectangleDiv15} />
+          <div className={styles.rectangleDiv16} />
+        </div>
+        <div className={styles.groupDiv29}>
+          <div className={styles.rectangleDiv17} />
+          <div className={styles.rectangleDiv18} />
+          <div className={styles.rectangleDiv19} />
+          <div className={styles.rectangleDiv20} />
+          <div className={styles.rectangleDiv21} />
+          <div className={styles.rectangleDiv22} />
+          <div className={styles.rectangleDiv23} />
+        </div>
+        <img
+          className={styles.elderManWithCaneFallingDoIcon}
+          alt=""
+          src="../eldermanwithcanefallingdown-1-2.svg"
+        />
+        <img
+          className={styles.analysisOfDataBlockchain1Icon}
+          alt=""
+          src="../analysisofdatablockchain-1.svg"
+        />
+        <img className={styles.vectorIcon2} alt="" src="../vector.svg" />
+        <img
+          className={styles.oldWomanSufferingFromHeadaIcon}
+          alt=""
+          src="../oldwomansufferingfromheadacheinhospital-1.svg"
+        />
+        <img
+          className={styles.retiredWomenFellDownByHeaIcon}
+          alt=""
+          src="../retiredwomenfelldownbyheartattack-1.svg"
+        />
+        <img
+          className={styles.oldPersonSmoking11}
+          alt=""
+          src="../oldpersonsmoking-1-1.svg"
+        />
+        <img className={styles.icon} alt="" src="../3648586-1@2x.png" />
+        <img className={styles.icon1} alt="" src="../2565-1.svg" />
+        <img className={styles.groupIcon6} alt="" src="../group.svg" />
+        <img className={styles.calender1Icon} alt="" src="../calender-1.svg" />
+        <img
+          className={styles.untitled11Icon}
+          alt=""
+          src="../untitled1-1.svg"
+        />
+        <div className={styles.navBarDiv}>
+          <div className={styles.cONTACTDiv}>CONTACT</div>
+          <div className={styles.sENIORDiv}>SENIORS</div>
+          <div className={styles.pRICINGDiv}>PRICING</div>
+          <div className={styles.tESTIMONIALSDiv}>TESTIMONIALS</div>
+          <Link to="/competitors"><div className={styles.cOMPETITORSDiv}>COMPETITORS</div></Link>
+          <div className={styles.pRODUCTDiv}>PRODUCT</div>
+          <div className={styles.cOMPANYDiv}>COMPANY</div>
+          <Link to="/"><div className={styles.lOGOB}>
+            <img src="logo.png" height="50" width="50"/></div></Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForSeniors;
